5a212f2125d05cb55797d2b866255b3d75146066
AYYANARK <aayanmasters@gmail.com>
2023-10-26 19:12:22 -0400

    perfect training files

--- training.py
+++ training.py
@@ -1,18 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
 import numpy as np
 from keras.preprocessing import image
 from matplotlib import pyplot
 
+
+
+# In[2]:
+
+
 img_width,img_height =800,600
 train_data_dir='dataset/train'
 validation_data_dir='dataset/test'
 batch_size=8
 
+
+# In[3]:
+
+
 if K.image_data_format() == 'channels_first' :
     input_shape = (3,img_width,img_height)
 else:
     input_shape = (img_width,img_height,3)
+
+
+# In[4]:
+
 
 train_datagen = ImageDataGenerator(
      rescale=1.0/255,
@@ -28,11 +47,11 @@ train_datagen = ImageDataGenerator(
     brightness_range=[0.5, 1.5] 
 )
 
-test_datagen = ImageDataGenerator(rescale=1. / 255,
-				  rotation_range=40,
-				brightness_range=[0.5, 1.5] ,
-				shear_range=0.2,
-    				zoom_range=0.2)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+# In[6]:
+
 
 train_generator = train_datagen.flow_from_directory(
      train_data_dir,
@@ -47,6 +66,7 @@ validation_generator = test_datagen.flow_from_directory(
      class_mode='categorical')
 
 
+# In[ ]:
 
 
 
